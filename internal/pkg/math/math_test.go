b13032a9f013cba002b80317ba86d563c5890658
Felix Kästner <mail@felix-kaestner.com>
2022-03-12 14:48:39 +0100

    add unit tests

--- internal/pkg/math/math_test.go
+++ internal/pkg/math/math_test.go
@@ -1,6 +1,8 @@
 package math
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/felix-kaestner/calculator/internal/pkg/assert"
@@ -29,4 +31,18 @@ func TestDivision(t *testing.T) {
 	res, err := Solve(proto.Method_DIV, 3.0, 2.0)
 	assert.Nil(t, err)
 	assert.Equal(t, 1.5, res)
+}
+
+func TestDivisionByZero(t *testing.T) {
+	_, err := Solve(proto.Method_DIV, 3.0, 0.0)
+	if !errors.Is(err, ErrDivisionByZero) {
+		t.Errorf("Test %s: Expected `%v` (type %v), Received `%v` (type %v)", t.Name(), ErrDivisionByZero, reflect.TypeOf(ErrDivisionByZero), err, reflect.TypeOf(err))
+	}
+}
+
+func TestInvalidMethod(t *testing.T) {
+	_, err := Solve(4, 1.0, 2.0)
+	if !errors.Is(err, ErrInvalidMethod) {
+		t.Errorf("Test %s: Expected `%v` (type %v), Received `%v` (type %v)", t.Name(), ErrInvalidMethod, reflect.TypeOf(ErrInvalidMethod), err, reflect.TypeOf(err))
+	}
 }
